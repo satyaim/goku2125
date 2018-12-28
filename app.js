16234e0f78d7042ce0cfb9a667d3d6748cedfaef
satyaim <thisissatyavrat@gmail.com>
2018-12-29 02:44:50 +0530

    Include semi colon error eslint

--- app.js
+++ app.js
@@ -2,14 +2,14 @@
 const keys = require('./config.js');
 const TelegramBot = require('node-telegram-bot-api');
 const rp = require('request-promise');
-const cheerio = require('cheerio')
-const ra = require('random-useragent')
+const cheerio = require('cheerio');
+const ra = require('random-useragent');
 const token = keys.telegramKey;
 const bot = new TelegramBot(token, {polling: true});
 
 const greet = [[ 'French','Spanish','German','Italian','Portugese','Hindi','Persian','Russian','Japanese','Korean', 'Turkish', 'Bahasa Indonesia'],
   ['BONJOUR','HOLA','HALLO','CIAO','OLÀ','NAMASTE','SALAAM','ZDRAS-TVUY-TE','OHAYO','AHN-YOUNG-HA-SE-YO', 'MERHABA', 'HALO']];
-const greetings = []
+const greetings = [];
 
 for(let iter = 0; iter<greet[0].length; iter++){
   greetings.push({
@@ -19,7 +19,7 @@ for(let iter = 0; iter<greet[0].length; iter++){
     input_message_content: {
       message_text : greet[1][iter]
     }
-  })
+  });
 }
 // Matches "/echo [whatever]"
 bot.onText(/\/echo (.+)/, (msg, match) => {
@@ -49,15 +49,15 @@ bot.onText(/\/song/, (msg, match) => {
     bot.sendMessage(chatId, 'Example : /song I See Fire');
     return;
   }
-  const searchURL = keys.songSearchURL+songName
+  const searchURL = keys.songSearchURL+songName;
 
-  console.log(match)
+  console.log(match);
 
   rp({uri : searchURL, json : true, headers: { 'User-Agent': ra.getRandom() }})
     .then(function(html){
       console.log(searchURL);
       const result = JSON.parse(html.substring(html.indexOf('(')+1, html.length-1));
-      console.log(result)
+      console.log(result);
       // Populate Reply With Songs as options
       let reply = 'Select Song to Download\n\n';
       const length = result.results.length;
@@ -65,20 +65,20 @@ bot.onText(/\/song/, (msg, match) => {
         bot.sendMessage(chatId, 'No Song Found For Your Query');
         return;
       }
-      console.log(length)
+      console.log(length);
       let iter = 0;
       while(iter<length){
         const artistString = result.results[iter].artist? 'Artist: ' + result.results[iter].artist + '\n' : '';
         reply += ( (iter+1).toString() + '. ' + result.results[iter].title + '\n' + artistString + result.results[iter].description + '\n\n');
         iter += 1;
-        console.log(reply)
+        console.log(reply);
       }
       // Populate Custom Keyboard
       iter=0;
       let keyboard = [];
       const size = parseInt(Math.sqrt(length));
-      console.log('size'+size)
-      let rowboard, col
+      console.log('size'+size);
+      let rowboard, col;
       while(iter<size){
         rowboard = [];
         col =0 ;
@@ -96,7 +96,7 @@ bot.onText(/\/song/, (msg, match) => {
         iter++;
       }
       keyboard.push(rowboard);
-      console.log(keyboard)
+      console.log(keyboard);
       // Send fetched Data
       bot.sendMessage(chatId, reply,
         { reply_markup : 
@@ -109,16 +109,16 @@ bot.onText(/\/song/, (msg, match) => {
       )
         .then(function(sent){
           bot.once('message', function(response){
-            console.log(sent)
+            console.log(sent);
             const choice = parseInt(response.text);
             const songURL = keys.songSongURL+result.results[choice-1].url;
             rp({uri : songURL, json : true, headers: { 'User-Agent': ra.getRandomData() }})
               .then(function(html){
                 if(JSON.parse(html.substring(html.indexOf('(')+1, html.length-1))){
-                  const songObj = JSON.parse(html.substring(html.indexOf('(')+1, html.length-1))
-                  console.log(songObj.song)
-                  const { title,artist,album,url,date,albumart } = songObj.song
-                  console.log(albumart)
+                  const songObj = JSON.parse(html.substring(html.indexOf('(')+1, html.length-1));
+                  console.log(songObj.song);
+                  const { title,artist,album,url,date,albumart } = songObj.song;
+                  console.log(albumart);
                   bot.sendAudio( chatId, url, {
                     caption : album+', '+date, 
                     thumb : albumart,
@@ -126,26 +126,26 @@ bot.onText(/\/song/, (msg, match) => {
                     title : title
                   }, 
                   function(err, res){
-                    console.log(err)
-                    console.log(res)
+                    console.log(err);
+                    console.log(res);
                     bot.sendMessage(chatId, 'Sorry! Error Occured');
                   }
-                  )
+                  );
                 }
               })
               .catch(function(err){
-                console.log(Object.keys(err))
+                console.log(Object.keys(err));
                 bot.sendMessage(chatId, 'Sorry! Error Occured');
               });
-          })
+          });
         })
         .catch(function(err){
-          console.log(err)
+          console.log(err);
           bot.sendMessage(chatId, 'Sorry! Error Occured');
-        })
+        });
     })
     .catch(function(err){
-      console.log(Object.keys(err))
+      console.log(Object.keys(err));
       bot.sendMessage(chatId, 'Sorry! Error Occured');
       //console.log(err.response)
       //handle error
@@ -159,10 +159,10 @@ bot.onText(/\/vs/, (msg, match) => {
   const vs = msg.text.substring(4);
   let vs1 = vs.split(' ')[0];
   let vs2 = vs.split(' ')[1];
-  let reply
+  let reply;
   const URL = 'https://www.google.com/search?q='+vs1+'+vs+'+vs2;
 
-  console.log(match)
+  console.log(match);
 
   if(!vs1 || !vs2){
     bot.sendMessage(chatId, 'Example : /vs ind aus');
@@ -175,13 +175,13 @@ bot.onText(/\/vs/, (msg, match) => {
       reply = reply.split(' results   ')[2].split('hours ago')[0].split('All times are in')[0].substring(0,300);
       console.log(reply);
       if(!reply)
-        reply= 'Sorry! Coundn\'t find result.'
+        reply= 'Sorry! Coundn\'t find result.';
       bot.sendMessage(chatId, reply); 
     })
     .catch(function(err){
-      reply= 'Sorry! Coundn\'t find result.'
+      reply= 'Sorry! Coundn\'t find result.';
       bot.sendMessage(chatId, reply);
-      console.log(err)
+      console.log(err);
     });
 
 
@@ -191,11 +191,11 @@ bot.onText(/\/livecri/, (msg, match) => {
   const chatId = msg.chat.id;
   const URL = 'https://www.cricbuzz.com/cricket-match/live-scores';
 
-  console.log(match)
+  console.log(match);
 
   rp(URL)
     .then(function(html){
-      let data
+      let data;
       data = html.replace(/<\/?[^>]+(>|$)/g, ' ').split('All  Domestic & Others')[1].split('Featured Videos')[0];
       data = data.replace(/ Live Score {2}Scorecard {2}Full Commentary {2}News {6}/g, 'STARTED <- MATCH\n\n');
       data = data.replace(/ {4}Read Preview {9}Match Facts {2}News {6}/g, 'YET TO START <- MATCH\n\n'); 
@@ -216,7 +216,7 @@ bot.onText(/\/livecri/, (msg, match) => {
       iter=0;
       let keyboard = [];
       const size = parseInt(Math.sqrt(length));
-      let rowboard, col
+      let rowboard, col;
       while(iter<size){
         rowboard = [];
         col =0 ;
@@ -246,17 +246,17 @@ bot.onText(/\/livecri/, (msg, match) => {
       )
         .then(function(sent){
           bot.once('message', function(response){
-            console.log(sent)
+            console.log(sent);
             const choice = parseInt(response.text);
             reply = '';
             reply += (data.split('<- MATCH')[choice-1]);
             reply += '\n\nMore Info : https://cricbuzz.com' + $('h3').find('a')[choice-1].attribs.href ;
             bot.sendMessage(chatId, reply);
-          })
-        })
+          });
+        });
     })
     .catch(function(err){
-      console.error(err)
+      console.error(err);
       const reply= 'Sorry! Coundn\'t find result.';
       bot.sendMessage(chatId, reply);
     });
@@ -266,20 +266,20 @@ bot.onText(/\/livecri/, (msg, match) => {
 
 bot.on('inline_query', function(query){
   if(!query.query)
-    bot.answerInlineQuery(query.id, [{ type : 'article', id : 'goku', title : 'GoKu', input_message_content: {message_text : ' Go<3Ku'}}])
+    bot.answerInlineQuery(query.id, [{ type : 'article', id : 'goku', title : 'GoKu', input_message_content: {message_text : ' Go<3Ku'}}]);
   if(query.query == 'cri'){
     let results = [];
     const URL = 'https://www.cricbuzz.com/cricket-match/live-scores';
     rp(URL)
       .then(function(html){
-        let data
+        let data;
         data = html.replace(/<\/?[^>]+(>|$)/g, ' ').split('All  Domestic & Others')[1].split('Featured Videos')[0];
         data = data.replace(/ Live Score {2}Scorecard {2}Full Commentary {2}News {6}/g, 'LIVE <- MATCH\n\n');
         data = data.replace(/ {4}Read Preview {9}Match Facts {2}News {6}/g, 'YET TO START <- MATCH\n\n'); 
         data = data.replace(/&nbsp;/g,' ');
         data = data.replace(/&#8226;/g,' ');
         data = data.replace(/ {4}/g,' ');
-        console.log(data)
+        console.log(data);
 
         let $ = cheerio.load(html);
         const length = $('h3').find('a').length;
@@ -295,20 +295,20 @@ bot.on('inline_query', function(query){
           });
           iter++;
         }
-        console.log(results)
-        bot.answerInlineQuery(query.id, results)
+        console.log(results);
+        bot.answerInlineQuery(query.id, results);
       })
       .catch(function(err){
-        console.log(err)
-      })
+        console.log(err);
+      });
   }
   if(query.query=='greet'){
-    bot.answerInlineQuery(query.id, greetings)
+    bot.answerInlineQuery(query.id, greetings);
   }
-})
+});
 
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
-  console.log(chatId)
+  console.log(chatId);
   //bot.sendMessage(chatId, 'Received your message');
 });	
