6806b8b55703b8b856475af4a959cefe6430a163
satyaim <thisissatyavrat@gmail.com>
2018-12-29 17:14:02 +0530

    include config

---
+++ config.js
@@ -0,0 +1,7 @@
+const keys = {
+  'telegramKey' : '718861791:AAGgKLZxvDhWPu135thP6ilN6AvzIqnIseY',
+  'songSearchURL' : 'https://databrainz.com/api/search_api.cgi?jsoncallback=jQuery111107201529123778425_1545983902766&format=json&qry=',
+  'songSongURL' : 'https://databrainz.com/api/data_api_new.cgi?jsoncallback=jQuery1111010160054108665384_1546013559628&r=new&format=json&id='
+};
+
+module.exports = keys;
